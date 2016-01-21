fd71335adb4517beb6a792a62cc352579fa715f5
Joshua Sziede <jsziede@yahoo.com>
2016-01-20 19:52:56 -0500

    Add Employee class. Update program to use Employee.

---
+++ cis237inclass1/Employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass1
+{
+    class Employee //noun class
+    {
+        //fields
+          //use private variables
+        private string firstName;
+        private string lastName;
+        private decimal weeklySalary; //use decimal when working with finance
+
+        //properties
+          //use public variables
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public decimal WeeklySalary
+        {
+            get { return weeklySalary; }
+            set { weeklySalary = value; }
+        }
+
+        public override string ToString()
+        {
+            return this.firstName + " " + this.lastName + " " + this.weeklySalary.ToString("C");
+        }
+
+        //method
+        public string GetFullName()
+        {
+            return this.firstName + " " + this.lastName;
+        }
+
+        //3 parameter constructor
+        public Employee(string firstName, string lastName, decimal weeklySalary)
+        {
+            //assign the passed in values to the fields
+            this.firstName = firstName;
+            this.lastName = lastName;
+
+            //assign the passed in value by the property
+            this.WeeklySalary = weeklySalary;
+        }
+
+        //default constructor
+        public Employee()
+        {
+            //let's just leave this blank
+        }
+    }
+}
--- cis237inclass1/Program.cs
+++ cis237inclass1/Program.cs
@@ -10,9 +10,50 @@ namespace cis237inclass1
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Here is some text");
-            Console.WriteLine("Here is some more text");
+            Employee employee1 = new Employee("Joshua", "Sziede", 123.45m);
+            Employee employee2 = new Employee("First", "Last", 321.00m);
+
+            int myNumber = 5;
+            Console.WriteLine(myNumber);
+            changeAnInt(myNumber);
+            Console.WriteLine(myNumber);
+
+            Console.WriteLine(employee1.ToString());
+            changeAnObject(employee1);
+            Console.WriteLine(employee1.ToString());
+
+            //Console.WriteLine(employee.GetFullName());
+            //Console.WriteLine(employee.ToString());\
+
+            //showing how to use an array with objects
+            Employee[] employees = new Employee[10];
+
+            //instanciate some employees into the array
+            employees[0] = new Employee("James", "Kirk", 453.00m);
+            employees[1] = new Employee("Jean-Luc", "Picard", 290.00m);
+            employees[2] = new Employee("Benjamin", "Sisko", 587.00m);
+            employees[3] = new Employee("Kathryn", "Janeway", 194.00m);
+            employees[4] = new Employee("Johnathan", "Archer", 394.00m);
+
+            foreach (Employee employee in employees)
+            {
+                if (employee != null)
+                {
+                    Console.WriteLine(employee.ToString());
+                }
+            }
+        }
+
+        //value
+        static void changeAnInt(int myNumber)
+        {
+            myNumber = 456;
+        }
+
+        //reference
+        static void changeAnObject(Employee employee)
+        {
+            employee.FirstName = "Thor";
         }
     }
 }
